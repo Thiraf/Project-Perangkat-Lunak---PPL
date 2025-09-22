91393bcf6939875776def09197c9154241747c04
ananda hirzi thirafi <anandaafie@gmail.com>
2025-09-22 12:27:01 +0700

    Update Name File

--- resources/js/Components/Foldercard.jsx
+++ resources/js/Components/Foldercard.jsx
@@ -1,16 +1,38 @@
 import ActionMenu from "@/Components/ActionMenu";
 
-export default function FolderCard({ id, name, type, onDelete, onRename, onFolderClick, onShare, canEdit }) {
+export default function FolderCard({
+    id,
+    name,
+    type,
+    onDelete,
+    onRename,
+    onFolderClick,
+    onShare,
+    canEdit,
+}) {
     return (
-        <div 
+        <div
             className="flex justify-between items-center border rounded-lg px-4 py-3 bg-white shadow-sm hover:shadow-md cursor-pointer"
             onClick={onFolderClick}
         >
             <span className="font-medium">{name}</span>
-            {canEdit
-            ? <ActionMenu id={id} name={name} type={type} onDelete={onDelete} onRename={onRename} onShare={onShare} />
-            : <img src="/images/share.png" alt="Shared" className="w-4 h-4 ml-2" title="Shared with you" />
-            }
+            {canEdit ? (
+                <ActionMenu
+                    id={id}
+                    name={name}
+                    type={type}
+                    onDelete={onDelete}
+                    onRename={onRename}
+                    onShare={onShare}
+                />
+            ) : (
+                <img
+                    src="/images/share.png"
+                    alt="Shared"
+                    className="w-4 h-4 ml-2"
+                    title="Shared with you"
+                />
+            )}
         </div>
     );
 }
